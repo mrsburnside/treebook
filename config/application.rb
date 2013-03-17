ee1e16388ca112fe7abeda9f1ada14ebab32e48b
mrsburnside <linda@burnsidebiz.com>
2013-03-17 14:09:15 -0400

    Install devise, add a user, and install devise views.

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Treebook
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #do not access database or load moduels when precompiling assets
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
